0c88d36839566fc9c1bfdf76f271b25b2c02fee0
Mohith Sai Gorla <46181469+iammohith@users.noreply.github.com>
2026-02-21 16:27:22 +0530

    Delete tests directory

--- tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Set dummy model paths before any imports so the loaders don't crash
-os.environ["MODEL_PATH"] = "dummy_model.joblib"
-os.environ["PREPROCESSOR_PATH"] = "dummy_preprocessor.joblib"
